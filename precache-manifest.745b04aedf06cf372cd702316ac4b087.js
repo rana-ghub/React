fd8774d5605918a12f8a1f74d6aa8e205a42b223
rana-ghub <ssshashankrana001@gmail.com>
2020-06-24 03:49:31 +0530

    Updates

---
+++ precache-manifest.745b04aedf06cf372cd702316ac4b087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e24ca920a90f6104b6a655d8129483d",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "d49d3990f3d2c249d593",
+    "url": "/React/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "0fb6c7d8ecc540eb092e",
+    "url": "/React/static/js/2.cf52e512.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.cf52e512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d49d3990f3d2c249d593",
+    "url": "/React/static/js/main.7f4f082d.chunk.js"
+  },
+  {
+    "revision": "98a6e4a32bf06b662974",
+    "url": "/React/static/js/runtime-main.ad7ee239.js"
+  }
+]);
